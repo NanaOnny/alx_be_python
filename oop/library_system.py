33a7a24d38f20d04318ff0b2964ed3468a609c74
nana <nanaonny@outlook.com>
2024-07-21 19:48:56 +0000

    all changes

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,3 +1,4 @@
+# library_system.py
 class Book:
     def __init__(self, title, author):
         self.title = title
@@ -10,12 +11,10 @@ class EBook(Book):
         super().__init__(title, author)
         self.file_size = file_size
 
-
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
         super().__init__(title, author)
         self.page_count = page_count
-
 
 class Library:
     def __init__(self):
@@ -36,9 +35,8 @@ class Library:
                 book_type = "Unknown"
 
             if isinstance(book, EBook):
-                print(f"{book_type}: {book.title} by {book.author}, File Size: {book.file_size} KB")
+                print(f"EBook: {book.title} by {book.author}, File Size: {book.file_size}KB")
             elif isinstance(book, PrintBook):
-                print(f"{book_type}: {book.title} by {book.author}, Page Count: {book.page_count}")
+                print(f"PrintBook: {book.title} by {book.author}, Page Count: {book.page_count}")
             else:
                 print(f"{book_type}: {book.title} by {book.author}")
-                
